a1e736f241ce07bb8ba8ef30e65827a3bfdbf6fc
satyamsinghal-rx <satyam.singhal@remotestate.com>
2025-03-26 15:05:18 +0530

    bug fixed

--- src/app/folders/[folderId]/notes/[noteId]/page.tsx
+++ src/app/folders/[folderId]/notes/[noteId]/page.tsx
@@ -5,12 +5,13 @@ import NoteEditor from "@/components/NoteEditor/NoteEditor";
 import NotesList from "@/components/NotesList/NotesList";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+type PageProps<T = unknown> = {
+  params: T;
+  searchParams?: Record<string, string | string[] | undefined>;
+};
 
-function NotesPage({
-  params,
-}: {
-  params: { folderId?: string; noteId: string; view?: string }; 
-}) {
+function NotesPage({ params }: PageProps<{ folderId?: string; noteId: string; view?: string }>) 
+  {
   const { folderId, noteId, view } = (params); 
   console.log("Params in NotesPage:", { folderId, noteId, view });
 
